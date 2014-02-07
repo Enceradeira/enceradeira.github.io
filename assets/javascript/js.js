60efb207c5387d51585153623aa059e62e885050
Jorg Jenni <joerg.jenni@garaio.com>
2014-02-07 11:20:09 +0000

    position navHeader is now dynamically set

---
+++ assets/javascript/js.js
@@ -0,0 +1,7 @@
+﻿var positionNavHeader = function() {
+    var contentPosition = $("section#content").position();
+    $("header#navHeader").offset({ top: 30, left: contentPosition.left });
+};
+
+$(window).resize(positionNavHeader);
+$(window).ready(positionNavHeader);

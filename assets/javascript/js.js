37ebad3a4208b55e3bf38986242dca54f82720e4
Jorg Jenni <joerg.jenni@garaio.com>
2014-02-07 13:12:54 +0000

    scrolling to sections when clicking on nav-links

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,7 +1,25 @@
 ﻿var positionNavHeader = function() {
     var contentPosition = $("section#content").position();
-    $("header#navHeader").offset({ top: 30, left: contentPosition.left });
+    // $("header#navHeader").offset({ top: 30, left: contentPosition.left });
+    
+    $("header#navHeader").css({
+        position: 'fixed',
+        top: 30,
+        left: contentPosition.left
+    });
 };
 
 $(window).resize(positionNavHeader);
 $(window).ready(positionNavHeader);
+
+$(window).ready(function() {
+    $("#main-nav a").click(function(evt) {
+        var link = $(evt.target);
+        var hash = link.data("hash");
+        var offset = $("#" + hash).offset();
+        $('html, body').animate({
+            scrollTop: offset.top,
+            scrollLeft: offset.left
+        });
+    });
+});

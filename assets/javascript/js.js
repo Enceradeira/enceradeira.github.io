6d2530d94bf6ebde40ccd1cf4a2dac74ff78a973
Jorg Jenni <joerg.jenni@garaio.com>
2014-02-07 14:24:44 +0000

    scrolling to section adjusted

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -17,9 +17,36 @@ $(window).ready(function() {
         var link = $(evt.target);
         var hash = link.data("hash");
         var offset = $("#" + hash).offset();
+        var scrollFineAdjustment = 0;
+        switch(hash) {
+            case "home":
+                scrollFineAdjustment = -31;
+                break;
+            case "about":
+                scrollFineAdjustment = -82;
+                break;
+            case "clients":
+                scrollFineAdjustment = -73;
+                break;
+            case "services":
+                scrollFineAdjustment = -109;
+                break;
+            case "work":
+                scrollFineAdjustment = -31;
+                break;
+            case "connect":
+                scrollFineAdjustment = -94;
+                break;
+            case "contact":
+                scrollFineAdjustment = -235;
+                break;
+                
+                
+        }
         $('html, body').animate({
-            scrollTop: offset.top,
-            scrollLeft: offset.left
+            scrollTop: (offset.top + scrollFineAdjustment) + "px"
         });
+        
+
     });
 });

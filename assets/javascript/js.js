9b0cdc657f6f067ded9e3e0f84e3a97e94db21eb
Jorg Jenni <joerg.jenni@garaio.com>
2014-02-09 09:41:35 +0000

    navigation using up/down-buttons works now

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -9,44 +9,69 @@
     });
 };
 
+var scrollToArticel = function(articelId) {
+    var offset = $(articelId).offset();
+    var scrollFineAdjustment = 0;
+    switch (articelId) {
+        case "#home":
+            scrollFineAdjustment = -31;
+            break;
+        case "#about":
+            scrollFineAdjustment = -82;
+            break;
+        case "#clients":
+            scrollFineAdjustment = -73;
+            break;
+        case "#services":
+            scrollFineAdjustment = -109;
+            break;
+        case "#work":
+            scrollFineAdjustment = -31;
+            break;
+        case "#connect":
+            scrollFineAdjustment = -94;
+            break;
+        case "#contact":
+            scrollFineAdjustment = -235;
+            break;
+
+
+    }
+    $('html, body').animate({
+        scrollTop: (offset.top + scrollFineAdjustment) + "px"
+    });
+    window.location.hash = articelId;
+};
+
 $(window).resize(positionNavHeader);
 $(window).ready(positionNavHeader);
 
-$(window).ready(function() {
+$(window).ready(function () {
+    $("#exploreheader .upArrow").click(function () {
+        var location = window.location.hash;
+        if (location == null || location.length == 0) 
+            return; // we are on the top 
+       
+        var nextArticelId = $(location).prev().attr('id');
+        if (nextArticelId == undefined)
+            return; // we are on the top (#home)
+        scrollToArticel("#" + nextArticelId);
+    });
+    $("#exploreheader .downArrow").click(function () {
+        var nextArticelId;
+        var location = window.location.hash;
+        if (location == null || location.length == 0) {
+            nextArticelId = $("#content").first().attr('id');
+        } else {
+            nextArticelId = $(location).next().attr('id');
+        }
+        if (nextArticelId == undefined)
+            return; // we are on the bottom
+        scrollToArticel("#" + nextArticelId);
+    });
     $("#main-nav a").click(function(evt) {
         var link = $(evt.target);
-        var hash = link.data("hash");
-        var offset = $("#" + hash).offset();
-        var scrollFineAdjustment = 0;
-        switch(hash) {
-            case "home":
-                scrollFineAdjustment = -31;
-                break;
-            case "about":
-                scrollFineAdjustment = -82;
-                break;
-            case "clients":
-                scrollFineAdjustment = -73;
-                break;
-            case "services":
-                scrollFineAdjustment = -109;
-                break;
-            case "work":
-                scrollFineAdjustment = -31;
-                break;
-            case "connect":
-                scrollFineAdjustment = -94;
-                break;
-            case "contact":
-                scrollFineAdjustment = -235;
-                break;
-                
-                
-        }
-        $('html, body').animate({
-            scrollTop: (offset.top + scrollFineAdjustment) + "px"
-        });
-        
-
+        var articelName = link.data("hash");
+        scrollToArticel("#" + articelName);
     });
 });

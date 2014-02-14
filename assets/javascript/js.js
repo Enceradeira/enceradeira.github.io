051e5f1a53d3d7ae1f0d715f3f23a495f096e5ea
Jorg Jenni <joerg.jenni@garaio.com>
2014-02-14 12:42:39 +0000

    blog-frog added and animated

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -59,9 +59,8 @@ var openSubnav2 = function () {
     $(this).siblings(".subnav2").show();
 };
 
-var showNewSign = function() {
+var animateNewSign = function() {
     var sign = $("#home #new_sign_background");
-    var position = sign.offset();
     var height = sign.height();
 
     sign.css('top', -height + "px");
@@ -74,6 +73,40 @@ var showNewSign = function() {
     });
 };
 
+var moveBlogFrog = function(blogfrog, position, way, continuation) {
+    blogfrog.animate({
+        left: (position + way) + "px"
+    }, 2500, "swing", continuation);
+};
+
+var moveBlogFrogAround = function (blogfrog, position, way, continuation) {
+    
+    var waitTime = Math.floor((Math.random() * 4000) + 4000);
+    moveBlogFrog(blogfrog, position, -way, function () {
+        setTimeout(function () {
+            moveBlogFrog(blogfrog, position - way, way, function () {
+                setTimeout(function() {
+                    moveBlogFrog(blogfrog, position, -way, function() { continuation(blogfrog, position, way,continuation); });
+                }, 500);
+            });
+        }, waitTime);
+    });
+};
+
+var animateBlogFrog = function() {
+    var blogfrog = $("#home #blogfrog");
+    var left = parseInt(blogfrog.css('left').replace(/px/,""));
+    var way = 55;
+    var rightPosition = left + way;
+   
+    blogfrog.css('left', String(rightPosition) + "px");
+    
+    setTimeout(function () {
+        moveBlogFrogAround(blogfrog, rightPosition, way, moveBlogFrogAround);
+    }, 1000);
+
+
+};
 
 $(window).resize(positionNavHeader);
 $(window).ready(positionNavHeader);
@@ -109,5 +142,6 @@ $(window).ready(function () {
     hideSubnav2();
     $("#vertical-links li .mainLinkMe").click(openSubnav2);
 
-    showNewSign();
+    animateNewSign();
+    animateBlogFrog();
 });
